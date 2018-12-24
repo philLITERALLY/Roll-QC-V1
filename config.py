862718ac3e5bce3f280757459b244bc4c9ee240f
Philip McKinney <wpmckinney@gmail.com>
2018-12-24 18:39:27 +0000

    Pass Fail working

--- config.py
+++ config.py
@@ -1,18 +1,47 @@
 """This module contains any configuration variables required for the program to run"""
 
+# External Libraries
+import cv2
+
 # Camera Settings
 CAM_WIDTH = 1920
 CAM_HEIGHT = 1080
 CAM_FPS = 30
+FRAME_WIDTH = 1440
+FRAME_HEIGHT = 1080
 
-# Size Settings
-ACTUAL_WIDTH = 355  # mm
-ACTUAL_HEIGHT = 28  # mm
-PIXEL_WIDTH = 474
-PIXEL_HEIGHT = 45.2
+# Lane Settings
+LANE_COUNT = 3
+LANE_WIDTH = 480
+LANE_HEIGHT = 200
+LANE_WIDTH_START = [0, 480, 960] # Creating manually for now
+LANE_WIDTH_END = [480, 960, 1440]
+LANE_HEIGHT_START = [460, 460, 460]
+LANE_HEIGHT_END = [650, 650, 650]
+EDGE_GAP = 40 # How far roll has to be in to count
+
+# Size Settings - Calibration Settings
+ACTUAL_WIDTH = 265.0  # mm
+ACTUAL_HEIGHT = 33.0  # mm
+PIXEL_WIDTH = 345.0
+PIXEL_HEIGHT = 50.0
 WIDTH_RATIO = ACTUAL_WIDTH / PIXEL_WIDTH
 HEIGHT_RATIO = ACTUAL_HEIGHT / PIXEL_HEIGHT
 
+# Draw Settings
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+BLUE = (255, 0, 0)
+YELLOW = (0, 255, 255)
+
+# Threshold Settings
+WHITE_THRESH = 190
+MIN_AREA = 2000 # Pixels
+FAIL_WIDTH_LOW = 260.0 / WIDTH_RATIO # mm / ratio
+FAIL_WIDTH_HIGH = 300.0 / WIDTH_RATIO # mm / ratio
+FAIL_HEIGHT_LOW = 20.0 / HEIGHT_RATIO # mm
+FAIL_HEIGHT_HIGH = 38.0 / HEIGHT_RATIO # mm
 
 def dimension_calc(width, height):
     """This function takes the pixel width and using the defined ratios converts to mm"""
--- main.py
+++ main.py
@@ -14,44 +14,76 @@ info_logger.init()
 # Get camera stream
 CAPTURE = camera_setup.main()
 
-# Setup font
-FONT = cv2.FONT_HERSHEY_SIMPLEX
+# Setup cropped image width
+CAM_WIDTH_MIDPOINT = config.CAM_WIDTH / 2
+HALF_FRAME_WIDTH = config.FRAME_WIDTH / 2
+FRAME_WIDTH_START = CAM_WIDTH_MIDPOINT - HALF_FRAME_WIDTH
+FRAME_WIDTH_END = CAM_WIDTH_MIDPOINT + HALF_FRAME_WIDTH
+
+# Setup cropped image height
+CAM_HEIGHT_MIDPOINT = config.CAM_HEIGHT / 2
+HALF_FRAME_HEIGHT = config.FRAME_HEIGHT / 2
+FRAME_HEIGHT_START = CAM_HEIGHT_MIDPOINT - HALF_FRAME_HEIGHT
+FRAME_HEIGHT_END = CAM_HEIGHT_MIDPOINT + HALF_FRAME_HEIGHT
+
+# Setup bounding box
+LANE_X1 = config.LANE_WIDTH_START[0] - 10
+LANE_Y1 = config.LANE_HEIGHT_START[0] + config.EDGE_GAP
+LANE_X2 = config.LANE_WIDTH_END[config.LANE_COUNT - 1] + 10
+LANE_Y2 = config.LANE_HEIGHT_END[0] - config.EDGE_GAP
+SPLIT_X1 = config.LANE_WIDTH_START[1]
+SPLIT_X2 = config.LANE_WIDTH_END[1]
 
 # Define the codec and create VideoWriter object
-FOURCC = cv2.VideoWriter_fourcc(*'DIVX')
-OUT = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(
-    'D', 'I', 'V', 'X'), 10.0, (1920, 200))
-OUT2 = cv2.VideoWriter('output2.avi', cv2.VideoWriter_fourcc(
-    'D', 'I', 'V', 'X'), 10.0, (1920, 200), False)
+FOURCC = cv2.VideoWriter_fourcc(*'MJPG')
+OUT = cv2.VideoWriter('output.avi', FOURCC, 10.0, (1440, 1080))
 
-while 1:
+RUNONCE = 1
+
+while RUNONCE:
     # Take each FRAME
     _, FRAME = CAPTURE.read()
 
-    CROPPED = FRAME[450:650, 0:1920]
+    CROPPED = FRAME[FRAME_HEIGHT_START:FRAME_HEIGHT_END, FRAME_WIDTH_START:FRAME_WIDTH_END]
     GRAY = cv2.cvtColor(CROPPED, cv2.COLOR_BGR2GRAY)
-    _, THRESHOLD_IMG = cv2.threshold(GRAY, 190, 255, 0)
-    DRAW_IMG = CROPPED
+    _, THRESHOLD_IMG = cv2.threshold(GRAY, config.WHITE_THRESH, 255, 0)
 
-    # run opencv find contours, only external boxes
-    _, CONTOURS, _ = cv2.findContours(
-        THRESHOLD_IMG, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    CONTOURS = [cnt for cnt in CONTOURS if cv2.contourArea(cnt) > 2000]
+    ORIG_LANE_IMG = []
+    LANE_IMG = []
 
-    # for each contour draw a bounding box and order number
-    for cnt in CONTOURS:
-        x, y, w, h = cv2.boundingRect(cnt)
-        dimensions = config.dimension_calc(w, h)
+    for i in range(config.LANE_COUNT):
+        ORIG_LANE_IMG = CROPPED[config.LANE_HEIGHT_START[i]:config.LANE_HEIGHT_END[i], config.LANE_WIDTH_START[i]:config.LANE_WIDTH_END[i]]
+        THRESH_LANE_IMG = THRESHOLD_IMG[config.LANE_HEIGHT_START[i]:config.LANE_HEIGHT_END[i], config.LANE_WIDTH_START[i]:config.LANE_WIDTH_END[i]]
+        
+        # run opencv find contours, only external boxes
+        _, CONTOURS, _ = cv2.findContours(THRESH_LANE_IMG, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        CONTOURS = [cnt for cnt in CONTOURS if cv2.contourArea(cnt) > config.MIN_AREA]
 
-        cv2.rectangle(DRAW_IMG, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(DRAW_IMG, dimensions, (x, y), FONT, 1, (0, 0, 255), 2)
+        # for each contour draw a bounding box and dimensions
+        for cnt in CONTOURS:
+            x, y, w, h = cv2.boundingRect(cnt)
 
-    # cv2.imshow('THRESHOLD_IMG', THRESHOLD_IMG)
-    cv2.imshow('DRAW_IMG', DRAW_IMG)
+            if y > config.EDGE_GAP and (y + h) < (config.LANE_HEIGHT - config.EDGE_GAP) :
+                color = config.GREEN
 
-    # # write the flipped frame
-    OUT.write(DRAW_IMG)
-    OUT2.write(THRESHOLD_IMG)
+                pixel_dimensions = "{0}px x {1}px".format(w, h)
+                calc_dimensions = config.dimension_calc(w, h)
+
+                if w < config.FAIL_WIDTH_LOW or w > config.FAIL_WIDTH_HIGH or h < config.FAIL_HEIGHT_LOW or h > config.FAIL_HEIGHT_HIGH:
+                    color = config.RED
+
+                cv2.rectangle(ORIG_LANE_IMG, (x, y), (x+w, y+h), color, 2)
+                cv2.putText(ORIG_LANE_IMG, pixel_dimensions, (x, y + h), config.FONT, 1, config.BLUE, 2)
+                cv2.putText(ORIG_LANE_IMG, calc_dimensions, (x, y), config.FONT, 1, color, 2)
+
+    cv2.rectangle(CROPPED, (LANE_X1, LANE_Y1), (LANE_X2, LANE_Y2), config.YELLOW, 2)
+    cv2.rectangle(CROPPED, (SPLIT_X1, LANE_Y1), (SPLIT_X2, LANE_Y2), config.YELLOW, 2)
+    cv2.imshow('CROPPED', cv2.resize(CROPPED, (1280, 960)))
+
+    # write the frame
+    OUT.write(CROPPED)
+
+    # RUNONCE = 0
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
@@ -62,5 +94,4 @@ info_logger.shutdown()
 # Release everything if job is finished
 CAPTURE.release()
 OUT.release()
-OUT2.release()
 cv2.destroyAllWindows()
